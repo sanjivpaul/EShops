452548c868918abb74e512973fd6c2e61155954b
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-08-06 13:58:15 +0530

    initialise e-shops

---
+++ src/test/java/com/sanjiv/eshops/EShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjiv.eshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
