72ba6ae67d56bb6bf74736d62db154127c2a636d
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-08-07 15:45:08 +0530

    Implement IProductService interface all mehods

--- src/main/java/com/sanjiv/eshops/service/product/ProductService.java
+++ src/main/java/com/sanjiv/eshops/service/product/ProductService.java
@@ -1,4 +1,62 @@
 package com.sanjiv.eshops.service.product;
 
-public class ProductService {
+import com.sanjiv.eshops.model.Product;
+
+import java.util.List;
+
+public class ProductService implements IProductService {
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryId(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
 }
