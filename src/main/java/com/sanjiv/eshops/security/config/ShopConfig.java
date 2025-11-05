3c1d5870289af3751d16a301142966bdb2fceb67
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-11-06 00:00:41 +0530

    update security config files

--- src/main/java/com/sanjiv/eshops/security/config/ShopConfig.java
+++ src/main/java/com/sanjiv/eshops/security/config/ShopConfig.java
@@ -1,14 +1,47 @@
 package com.sanjiv.eshops.security.config;
 
+import com.sanjiv.eshops.security.jwt.AuthTokenFilter;
+import com.sanjiv.eshops.security.jwt.JwtAuthEntryPoint;
+import com.sanjiv.eshops.security.user.ShopUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class ShopConfig {
+    private final ShopUserDetailsService userDetailsService;
+    private final JwtAuthEntryPoint authEntryPoint;
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+       return authConfig.getAuthenticationManager();
+    }
+
+//    public DaoAuthenticationProvider daoAuthenticationProvider(){
+//
+//    }
 }
