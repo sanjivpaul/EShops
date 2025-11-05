9f803d2aae7654c980db76396bf71520ad199eb3
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-11-05 23:44:28 +0530

    update jwt codes for setting

--- src/main/java/com/sanjiv/eshops/security/jwt/JwtUtils.java
+++ src/main/java/com/sanjiv/eshops/security/jwt/JwtUtils.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
@@ -16,7 +17,10 @@ import java.util.List;
 
 @Component
 public class JwtUtils {
+    @Value("${auth.token.jwtSecret}")
     private String jwtSecret;
+
+    @Value("${auth.token.expirationInMils}")
     private int expirationTime;
 
 
